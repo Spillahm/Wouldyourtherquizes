143df7cfaad61ff2c3245ee65427f4bfd67cb393
FIRST_NAME LAST_NAME <example@example.com>
2023-04-15 14:40:48 -0500

    three more questions for would you rather

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Would You Rather</h1>
+
+    <would-you-rather v-for=" (question, index) in questions"
+                      :key="index"
+                      :question="question"
+                      v-on:answer-changed="answerChanged" >
+    </would-you-rather>
+
+    <p>{{userSelectionMessage}}</p>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRathequiz.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data() {
+    return {
+    questions: [
+      {
+        id:0,
+        Question: 'be a detective or a pilot?',
+        Answer1: 'be a pilot',
+        Answer2: 'be a detective',
+      },
+      {
+        id:1,
+        Question:'would you rather dance or sing?',
+        Answer1: 'I will sing',
+        Answer2: 'I will dance',
+      },
+      {
+       id:2,
+       Question: 'would you rather be a superhero or wizard?',
+       Answer1: 'Be a wizard',
+       Answer2: 'Be a superhero',
+      }
+    ],
+      userSelectionMessage:''
+    }
+  },
+
+  methods: {
+    answerChanged(choice) {
+      this.userSelectionMessage = `Thanks! you chose ${choice}`
+
+    }
   }
 }
 </script>
 
 <style>
+
+body {
+  background: darkkhaki;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Agency FB;
+
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: aqua;
+  margin-top: 70x;
+  background: azure;
 }
 </style>
---
+++ src/components/WouldYouRathequiz.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+
+    <h2>{{question.Question}}</h2>
+
+
+    <div>
+
+    <input type="radio" :id="question.id + '-answer1'" :value="question.Answer1" v-model="selectedAnswer">
+    <label :for="question.id + '-answer1'">{{question.Answer1}}</label>
+    </div>
+
+    <div>
+    <input type="radio" :id="question.id + '-answer2' " :value="question.Answer2" v-model="selectedAnswer">
+    <label :for="question.id + '-answer2'">{{question.Answer2}}</label>
+    </div>
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: {
+    question: {
+      type: Object,
+      required: true
+    }
+
+  },
+  data() {
+    return {
+      selectedAnswer: ''
+    }
+  },
+  watch: {
+    selectedAnswer(newValue){
+      this.$emit('answer-changed', newValue)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h2 {
+  margin: 40px 0 0;
+}
+
+.id1 {
+  border: 2x aqua;
+  font-family: Avenir;
+  background: aquamarine;
+  margin: 15px;
+
+
+
+}
+#data {
+  background-color: blue;
+}
+</style>
